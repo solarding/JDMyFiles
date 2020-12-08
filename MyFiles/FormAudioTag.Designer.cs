186129ebeb4b02544ad6e77c42913e9be2b69522
Jianyang Ding <jianyang.ding@soprasteria.com>
2020-12-08 18:33:48 +0100

    improvements

--- MyFiles/FormAudioTag.Designer.cs
+++ MyFiles/FormAudioTag.Designer.cs
@@ -30,31 +30,33 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lv = new System.Windows.Forms.ListView();
             this.ColumnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.btnLookupFolder = new System.Windows.Forms.Button();
             this.btnTags = new System.Windows.Forms.Button();
             this.btnFix = new System.Windows.Forms.Button();
             this.btnFormatFN = new System.Windows.Forms.Button();
+            this.chkMedia = new System.Windows.Forms.CheckBox();
+            this.cbSizeOption = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // listView1
+            // lv
             // 
-            this.listView1.Activation = System.Windows.Forms.ItemActivation.TwoClick;
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lv.Activation = System.Windows.Forms.ItemActivation.TwoClick;
+            this.lv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.ColumnHeader1});
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.listView1.GridLines = true;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(0, 65);
-            this.listView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(959, 478);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            this.lv.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lv.GridLines = true;
+            this.lv.HideSelection = false;
+            this.lv.Location = new System.Drawing.Point(0, 105);
+            this.lv.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(959, 438);
+            this.lv.TabIndex = 1;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            this.lv.View = System.Windows.Forms.View.Details;
+            this.lv.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
             // 
             // ColumnHeader1
             // 
@@ -99,7 +101,7 @@
             // 
             // btnFix
             // 
-            this.btnFix.Location = new System.Drawing.Point(644, 12);
+            this.btnFix.Location = new System.Drawing.Point(144, 45);
             this.btnFix.Name = "btnFix";
             this.btnFix.Size = new System.Drawing.Size(108, 26);
             this.btnFix.TabIndex = 8;
@@ -109,7 +111,7 @@
             // 
             // btnFormatFN
             // 
-            this.btnFormatFN.Location = new System.Drawing.Point(514, 11);
+            this.btnFormatFN.Location = new System.Drawing.Point(14, 45);
             this.btnFormatFN.Name = "btnFormatFN";
             this.btnFormatFN.Size = new System.Drawing.Size(124, 27);
             this.btnFormatFN.TabIndex = 9;
@@ -117,36 +119,68 @@
             this.btnFormatFN.UseVisualStyleBackColor = true;
             this.btnFormatFN.Click += new System.EventHandler(this.bntFormatFN_Click);
             // 
+            // chkMedia
+            // 
+            this.chkMedia.AutoSize = true;
+            this.chkMedia.Location = new System.Drawing.Point(274, 49);
+            this.chkMedia.Name = "chkMedia";
+            this.chkMedia.Size = new System.Drawing.Size(93, 21);
+            this.chkMedia.TabIndex = 10;
+            this.chkMedia.Text = "Media Files";
+            this.chkMedia.UseVisualStyleBackColor = true;
+            // 
+            // cbSizeOption
+            // 
+            this.cbSizeOption.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cbSizeOption.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbSizeOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSizeOption.FormattingEnabled = true;
+            this.cbSizeOption.Items.AddRange(new object[] {
+            "in Byte",
+            "in KB",
+            "in MB",
+            "in GB"});
+            this.cbSizeOption.Location = new System.Drawing.Point(401, 49);
+            this.cbSizeOption.Name = "cbSizeOption";
+            this.cbSizeOption.Size = new System.Drawing.Size(72, 25);
+            this.cbSizeOption.TabIndex = 11;
+            this.cbSizeOption.SelectedIndexChanged += new System.EventHandler(this.cbSizeOption_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(959, 543);
+            this.Controls.Add(this.cbSizeOption);
+            this.Controls.Add(this.chkMedia);
             this.Controls.Add(this.btnFormatFN);
             this.Controls.Add(this.btnFix);
             this.Controls.Add(this.btnTags);
             this.Controls.Add(this.btnLookupFolder);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lv);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Audio Tag";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lv;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button btnLookupFolder;
         private System.Windows.Forms.Button btnTags;
         private System.Windows.Forms.ColumnHeader ColumnHeader1;
         private System.Windows.Forms.Button btnFix;
         private System.Windows.Forms.Button btnFormatFN;
+        private System.Windows.Forms.CheckBox chkMedia;
+        private System.Windows.Forms.ComboBox cbSizeOption;
     }
 }
 
--- MyFiles/FormAudioTag.cs
+++ MyFiles/FormAudioTag.cs
@@ -12,19 +12,19 @@ namespace JD
         public Form1()
         {
             InitializeComponent();
-            comboBox1.Items.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));             
+            comboBox1.Items.Add(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            GenerateColumns(false);
         }
 
-        private void GenerateColumns(bool reset = false)
+        private void GenerateColumns(bool isMedia = false)
         {
-            if (reset)
-            {
-                listView1.Clear();
-                listView1.Columns.Add("File Name", 350);
-            }
-            listView1.Columns.Add("Title", 300);
-            listView1.Columns.Add("Artist", 150);
+            lv.Clear();
+            lv.Columns.Add("File Name", 350);
+            lv.Columns.Add("Size(K)", 80, HorizontalAlignment.Right);
+            if (!isMedia) return;
 
+            lv.Columns.Add("Title", 300);
+            lv.Columns.Add("Artist", 150);
         }
 
         private void btnLookupFolder_Click(object sender, EventArgs e)
@@ -33,29 +33,51 @@ namespace JD
             if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel) return;
             comboBox1.Text = folderBrowserDialog1.SelectedPath;
             comboBox1.Items.Add(comboBox1.Text);
-            LoadDir(true);
+            ReloadFiles();
+        }
+
+        private float _sizeOption = 1024.0f;
+        private void cbSizeOption_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var option = cbSizeOption.Text.ToUpper();
+            float newSizeOption;
+            if (option.EndsWith("GB"))
+                newSizeOption = 1024 * 1024 * 1024f;
+            else if (option.EndsWith("MB"))
+                newSizeOption = 1024 * 1024f;
+            else if (option.EndsWith("KB"))
+                newSizeOption = 1024f;
+            else
+                newSizeOption = 1;
+            if (Math.Abs(newSizeOption - _sizeOption)<0.001) return;
+            _sizeOption = newSizeOption;
+            lv.Columns[1].Text = $"Size({option})";
+            ReloadFiles();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            LoadDir();
+            ReloadFiles();
         }
 
         private void comboBox1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter) LoadDir();
+            if (e.KeyCode == Keys.Enter) ReloadFiles();
         }
 
-        private void LoadDir(bool readTag = false)
+        private void ReloadFiles()
         {
+            bool readTag = chkMedia.Checked;
+
             Cursor = Cursors.WaitCursor;
             if (readTag) GenerateColumns(true);
-            listView1.Items.Clear();
-            listView1.BeginUpdate();
+            lv.Items.Clear();
+            lv.BeginUpdate();
             var files = new DirectoryInfo(comboBox1.Text).GetFiles();
             foreach (var file in files)
             {
-                var item = listView1.Items.Add(file.Name, file.Extension.Substring(1).ToLower());
+                var item = lv.Items.Add(file.Name, string.IsNullOrEmpty(file.Extension) ? "" : file.Extension.Substring(1).ToLower());
+                item.SubItems.Add((file.Length/_sizeOption).ToString("#,#.###"));
                 if (!readTag)  continue;  
                 
                 try
@@ -66,20 +88,20 @@ namespace JD
                 }
                 catch { }
             }
-            listView1.EndUpdate();
+            lv.EndUpdate();
             Cursor = Cursors.Default;
         }
 
         private void btnTags_Click(object sender, EventArgs e)
         {
-            LoadDir(true);
+            ReloadFiles();
         }
 
         private void btnFix_Click(object sender, EventArgs e)
         {
             Cursor = Cursors.WaitCursor;
             
-            foreach (var xitem in listView1.Items)
+            foreach (var xitem in lv.Items)
             {
                 var item = (ListViewItem)xitem;
                 var file = Path.Combine(comboBox1.Text, item.Text);
@@ -99,7 +121,7 @@ namespace JD
         {
             Cursor = Cursors.WaitCursor;
 
-            foreach (var xitem in listView1.Items)
+            foreach (var xitem in lv.Items)
             {
                 var item = (ListViewItem)xitem;
                 var file = Path.Combine(comboBox1.Text, item.Text);
@@ -116,17 +138,17 @@ namespace JD
             }
             Cursor = Cursors.Default;
 
-            LoadDir(true);
+            ReloadFiles();
         }
 
         private void listView1_ItemActivate(object sender, EventArgs e)
         {
-            var filename = Path.Combine(comboBox1.Text, listView1.SelectedItems[0].Text);
+            var filename = Path.Combine(comboBox1.Text, lv.SelectedItems[0].Text);
             //if it is directory,go into
             if (Directory.Exists(filename))
             {
                 comboBox1.Text = filename;
-                LoadDir(true);
+                ReloadFiles();
                 return;
             }
 
@@ -138,6 +160,6 @@ namespace JD
 
         }
 
-       
+
     }
 }
